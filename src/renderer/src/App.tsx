651e993b7a1484987be975f9413a214642677b17
adam taoufiq <33988878+adamtao14@users.noreply.github.com>
2024-02-11 17:14:17 +0100

    ui part done, create,delete done

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,14 +1,30 @@
-import { ActionButtonsRow, Content, NotePreviewList, RootLayout, Sidebar } from '@/components'
+import {
+  ActionButtonsRow,
+  Content,
+  FloatingNoteTitle,
+  MarkdownEditor,
+  NotePreviewList,
+  RootLayout,
+  Sidebar
+} from '@/components'
+import { useRef } from 'react'
 
 const App = () => {
+  const contentContainerRef = useRef<HTMLDivElement>(null)
+  const resetScroll = () => {
+    contentContainerRef.current?.scrollTo(0, 0)
+  }
   return (
     <>
       <RootLayout>
         <Sidebar className="p-2">
           <ActionButtonsRow className="flex justify-between mb-2" />
-          <NotePreviewList className="mt-3 space-y-1" />
+          <NotePreviewList className="mt-3 space-y-1" onSelect={resetScroll} />
         </Sidebar>
-        <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
+        <Content ref={contentContainerRef} className="border-l bg-zinc-900/50 border-l-white/20">
+          <FloatingNoteTitle className="pt-2" />
+          <MarkdownEditor />
+        </Content>
       </RootLayout>
     </>
   )
